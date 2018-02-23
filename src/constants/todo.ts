22cf6991d56b5a98acd20ba8353cd36314567a23
Artsem Vasilevich <artsem.vasilevich@itechart-group.com>
2018-02-23 14:36:51 +0300

    initial

---
+++ src/constants/todo.ts
@@ -0,0 +1,8 @@
+export const ADD_TODO = 'ADD_TODO';
+export type ADD_TODO = typeof ADD_TODO;
+
+export const REMOVE_TODO = 'REMOVE_TODO';
+export type REMOVE_TODO = typeof REMOVE_TODO;
+
+export const UPDATE_TODO = 'UPDATE_TODO';
+export type UPDATE_TODO = typeof UPDATE_TODO;
