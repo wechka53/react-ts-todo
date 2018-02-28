e7f3a2c768c62d7ddb0aafcdc3c6755b009c327b
artsem.vasilevich <artsem.vasilevich@itechart-group.com>
2018-02-28 20:30:37 +0300

    added reselect

--- src/containers/TodoContaier.tsx
+++ src/containers/TodoContaier.tsx
@@ -6,22 +6,48 @@ import { addTodo, removeTodo, updateTodo } from 'actions/todo';
 
 import { TodoInterface, TodoActionInterface } from 'types/todo';
 import { RootState } from 'types/store';
-import { FilterTypes } from 'types/filter';
+import { Filter, FilterTypes } from 'types/filter';
+import { createSelector } from 'reselect';
 
-const filteredOutput = (todos: TodoInterface[], filterType: string): TodoInterface[] => {
-    switch (filterType) {
-        case FilterTypes.SHOW_ALL:
-            return todos;
-        case FilterTypes.SHOW_ACTIVE:
-            return todos.filter((todo: TodoInterface) => !todo.completed);
-        case FilterTypes.SHOW_COMPLETED:
-            return todos.filter((todo: TodoInterface) => todo.completed);
-        default:
-            throw Error(`Incorrect filter  ${filterType}`);
-    }
-};
 
-const mapStateToProps = (state: RootState) => ({todos: filteredOutput(state.todos, state.filter)});
+const getTodos = (state: RootState) => state.todos;
+const getFilterType = (state: RootState) => state.filter;
+
+const filterTodos = createSelector(
+    [getTodos, getFilterType],
+    (todos: TodoInterface[], filterType: Filter) => {
+        switch (filterType) {
+            case FilterTypes.SHOW_ALL:
+                console.log('pure');
+                return todos;
+            case FilterTypes.SHOW_ACTIVE:
+                console.log('pure');
+                return todos.filter((todo: TodoInterface) => !todo.completed);
+            case FilterTypes.SHOW_COMPLETED:
+                console.log('pure');
+                return todos.filter((todo: TodoInterface) => todo.completed);
+            default:
+                throw Error(`Incorrect filter  ${filterType}`);
+        }
+    });
+
+// const filteredOutput = (todos: TodoInterface[], filterType: string): TodoInterface[] => {
+//     switch (filterType) {
+//         case FilterTypes.SHOW_ALL:
+//             return todos;
+//         case FilterTypes.SHOW_ACTIVE:
+//             return todos.filter((todo: TodoInterface) => !todo.completed);
+//         case FilterTypes.SHOW_COMPLETED:
+//             return todos.filter((todo: TodoInterface) => todo.completed);
+//         default:
+//             throw Error(`Incorrect filter  ${filterType}`);
+//     }
+// };
+
+const mapStateToProps = (state: RootState) => (
+    {
+        todos: filterTodos(state),
+    });
 
 const mapDispatchToProps = (dispatch: Dispatch<TodoActionInterface>) => ({
     addTodo: (text: string) => dispatch(addTodo(text)),
