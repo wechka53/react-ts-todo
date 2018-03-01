c6f7effc2d8f51e66ef6fe8011f7ca1211d259e9
Artsem Vasilevich <artsem.vasilevich@itechart-group.com>
2018-03-01 17:17:27 +0300

    added badge

--- src/redusers.ts
+++ src/redusers.ts
@@ -2,6 +2,7 @@ import { combineReducers }   from 'redux';
 import { reducer as todos }  from 'redusers/todo';
 import { reducer as filter } from 'redusers/filter';
 
+asdasdasd
 export default combineReducers({
     todos,
     filter
